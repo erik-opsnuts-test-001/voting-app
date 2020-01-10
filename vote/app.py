41b2d661b4332087f0181e1064848461a36c62d7
erik landerholm <elanderholm@gmail.com>
2020-01-10 15:03:44 -0800

    down with google

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Google")
-option_b = os.getenv('OPTION_B', "Apple")
+option_a = os.getenv('OPTION_A', "Zynq")
+option_b = os.getenv('OPTION_B', "Google")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
