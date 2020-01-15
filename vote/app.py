836d9c2478f5471d7a9d62c1c348e9561ec1b54a
erik landerholm <elanderholm@gmail.com>
2020-01-14 16:59:59 -0800

    demooo test

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "ApiTracker")
-option_b = os.getenv('OPTION_B', "someguy")
+option_a = os.getenv('OPTION_A', "Google")
+option_b = os.getenv('OPTION_B', "Apple")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
