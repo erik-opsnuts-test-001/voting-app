4f7b75d7eec363b3f90680c0c6a87ffa182d5010
erik landerholm <elanderholm@gmail.com>
2020-03-10 11:14:50 -0700

    hello agora vs amazon

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Google")
-option_b = os.getenv('OPTION_B', "Apple")
+option_a = os.getenv('OPTION_A', "Hello Agora")
+option_b = os.getenv('OPTION_B', "Amazon")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
