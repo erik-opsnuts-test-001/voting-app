45e12261f300c84eb4b26292e994a4d399dcf958
erik landerholm <elanderholm@gmail.com>
2020-03-12 14:27:59 -0700

    getspect vs the world

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Google")
-option_b = os.getenv('OPTION_B', "Apple")
+option_a = os.getenv('OPTION_A', "GetSpect")
+option_b = os.getenv('OPTION_B', "TheWorld")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
